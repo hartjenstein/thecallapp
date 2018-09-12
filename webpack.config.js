7991e5dd1fdfa6322e44efbb1bd4a5eb08b43ba0
Hartjenstein <hartjenstein@gmail.com>
2018-09-12 12:20:14 +0200

    add gitignore

--- webpack.config.js
+++ webpack.config.js
@@ -31,6 +31,11 @@ module.exports = {
         swDest: 'sw.js',
         clientsClaim: true,
         skipWaiting: true,
+        runtimeCaching: [{
+          urlPattern: new RegExp('https://the-call.herokuapp.com'),
+          handler: 'staleWhileRevalidate'
+        }]
+      })
       })
     ],
     devServer: {
