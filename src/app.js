da1855202375dfed45dd9042f3930d517db14349
Hartjenstein <hartjenstein@gmail.com>
2018-09-12 15:50:13 +0200

    fix bug service workers

--- src/app.js
+++ src/app.js
@@ -4,18 +4,7 @@ import IndecisionApp from './components/indecisionApp';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
-function init() {
-  if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-      navigator.serviceWorker.register('/sw.js').then(registration => {
-        console.log('SW registered: ', registration);
-      }).catch(registrationError => {
-        console.log('SW registration failed: ', registrationError);
-      });
-    });
-  }
-}
-init();
+
 
 ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
 
